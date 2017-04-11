1452774cd50c8f2b1ed4ecd698bd0269b4731912
Adam <acdoyle630@gmail.com>
2017-04-11 11:14:24 -1000

    corrected dates, start at midnight

--- public/js/client.js
+++ public/js/client.js
@@ -2,7 +2,6 @@
 
 
 window.addEventListener('load', pageLoad);
-const today = document.querySelector('#day0');
 
 function pageLoad(){
   oReq = new XMLHttpRequest();
@@ -14,16 +13,32 @@ function pageLoad(){
 function display(){
   const requestData = JSON.parse(this.responseText);
   const weather = requestData.list;
-  console.log(weather);
+  const city = requestData.city.name;
+  console.log(requestData.list);
+  const todayDisplay = document.createElement('h1');
+  const today = document.querySelector('#city');
+  todayDisplay.innerHTML = city;
+  today.appendChild(todayDisplay);
+
+
   fiveDay(weather);
 }
 
 function fiveDay(arr){
-  for(var i = 0; i < 5; i++){
-    const current = arr[i].weather[0].description;
-    console.log(current);
-    const description = document.createElement('h1');
-    description.innerHTML = current;
+  for(var i = 0; i < 40; i+=8){
+    const currentDescription = arr[i].weather[0].description;
+    const today = document.querySelector('#day' + i/8);
+    const currentTemp = arr[i].main.temp;
+    const date = arr[i].dt_txt;
+    console.log(date);
+    const description = document.createElement('h3');
+    const currentTempDisplay = document.createElement('h3');
+    const currentDate = document.createElement('h2');
+    description.innerHTML = currentDescription;
+    currentTempDisplay.innerHTML = (currentTemp * 9/5 - 459.67).toFixed(0) + ' Degrees';
+    currentDate.innerHTML = date;
+    today.appendChild(currentDate);
     today.appendChild(description);
+    today.appendChild(currentTempDisplay);
   }
 }
